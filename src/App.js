ecc667a35d6d199a8cd25b7a7a3beac842fe5f7e
nschwags <nschwags@gmail.com>
2024-08-26 13:44:09 -0500

    Added all main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Main from "./Components/Main";
+import Nav from "./Components/Nav";
+import Menu from "./Components/Menu";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
   );
 }
 
